1623bbcd0e3f01e0367e758300382a7cdcf90b56
gpena1 <gpena1@guilford.edu>
2026-04-27 09:10:00 -0400

    fix video path

--- pages/1_Map.py
+++ pages/1_Map.py
@@ -5,7 +5,8 @@ st.title("🌍 Climate Visualization")
 st.markdown("### 🎥 Heatmap Animation")
 
 st.markdown("""
-This animation shows temperature changes over time across regions, highlighting increasing heat patterns and climate risk.
+This animation shows temperature changes over time across regions, 
+highlighting increasing heat patterns and climate risk.
 """)
 
-st.video("../capstoneZOOM.mp4")
+st.video("pages/capstoneZOOM.mp4")
